55c9b0e5697f157db47dc3e23d4f9ca02706677b
Iannick Gagnon <38300786+iangagn@users.noreply.github.com>
2021-12-15 12:20:41 -0500

    Create insertion_sort_increasing.py

---
+++ insertion_sort_increasing.py
@@ -0,0 +1,19 @@
+def insertion_sort_increasing(arr):
+  '''
+  Implementation of insertion sort.
+
+  Sorts in increasing order.
+
+  Parameters
+  ----------
+  arr : list, tuple or set
+      An array.
+      
+  Returns nothing.
+  '''
+
+  for i in range(1, len(arr)):
+    j = i
+    while arr[j] < arr[j - 1] and j > 0:
+      arr[j], arr[j - 1] = arr[j - 1], arr[j]
+      j -= 1
